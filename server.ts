ed5b3863b76aed03324e7b9f554e552dc4e92c11
FourMinato <kropgerken@gmail.com>
2026-03-18 18:45:11 +0700

    service-update

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import http from "http";
 import { app } from "./app";
 import dotenv from 'dotenv';
 dotenv.config();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
